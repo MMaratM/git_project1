aff739c0830fbd443e8831432cbc17f31bcfb162
MMaratM <75020565+MMaratM@users.noreply.github.com>
2020-11-25 15:04:35 +0300

    Update programm.py
    Someone may be i? added a new line

--- programm.py
+++ programm.py
@@ -1,8 +1,9 @@
 def main():
-    print("My first git programm")
+    print("My first git programm")  
     print('And I Change it every day')
     print("Trening")
-
+print("sdfsf"
+      
 if __name__ == '__main__':
     main()
 
